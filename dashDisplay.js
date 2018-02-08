1e89213201aeed5d0d395847d12a7e0457278d99
Jane Weesner <jane.e.weesner@gmail.com>
2018-02-08 16:06:09 -0600

    Add basic Formatting

--- dashDisplay.js
+++ dashDisplay.js
@@ -27,7 +27,7 @@ function checkNewMessages(){
   while (currStreamIndex >= index) {
     var tweet = streams.home[index];
     var $tweet = $('<section class = "tweet"></section>');
-    var content = `<a data-name=${tweet.user} href="#top">@${tweet.user}</a>`+ ': ' + tweet.message;
+    var content = `<a data-name=${tweet.user} href="#">@${tweet.user}</a>`+ ': ' + tweet.message;
     var time = tweet.created_at;
     if (userDetailName === tweet.user){
       var $userTweet = $('<section class = "userTweet"></section>');
@@ -48,22 +48,22 @@ $('.tweet a').on('click',function(){
   if ($('.userDetail').length !==0){
     $('.userDetail').remove();
   };
+  $(".feed").addClass("withUser");
+  $(".feed").after($("<div class='fix'><\div>)"));
   var userName=$(this).data('name');
   $userSection = $(`<aside class="userDetail" data-name=${userName}></aside>`);
   $userSection.prepend($(`<h2> ${userName} </h2>` ));
-  $userSection.hide();
-  $("#top").after($userSection);
+  $(".header").after($userSection);
   var userObj = {};
   userObj=streams.users[userName];
   for (var i=0; i<userObj.length; i++){
     var tweet = userObj[i];
-    var $tweet = $('<section class = "userTweet"></section>');
+    var $tweet = $('<section class = "userTweet tweet"></section>');
     var content = tweet.message;
     var time = tweet.created_at;
     $tweet.append($(`<div class="tweetContent"> ${content} </div>`));
     $tweet.append($(`<div class="tweetTime"> ${time} </div>`));
     ($(".userDetail h2")).after($tweet);
-  $userSection.slideDown("fast");
   }
   
 });
