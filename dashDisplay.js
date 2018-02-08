a5a52576194e8370e481bdab899cbeacb38ec0e3
Jane Weesner <jane.e.weesner@gmail.com>
2018-02-08 17:58:30 -0600

    Fix issue with delegation of handler

--- dashDisplay.js
+++ dashDisplay.js
@@ -5,7 +5,12 @@ $(document).ready(function () {
   var $body = $('body');
   var index = 0;
   var $feed = $(`<section class = "feed"></section>`);
+  $feed.append($("<h2>newsfeed</h2>"));
+  var $feedContent = $('<section class = "feedContent"></section>');
+  $feed.append($feedContent);
   $body.append($feed);
+  $body.append($("<div class='fix'><\div>)"))
+  ;
   checkNewMessages();
 
   
@@ -30,29 +35,29 @@ function checkNewMessages(){
     var content = `<a data-name=${tweet.user} href="#">@${tweet.user}</a>`+ ': ' + tweet.message;
     var time = tweet.created_at;
     if (userDetailName === tweet.user){
-      var $userTweet = $('<section class = "userTweet"></section>');
-      $userTweet.append($(`<div class="tweetContent">${tweet.message}</div>`));
-      $userTweet.append($(`<div class="tweetTime"> ${time} </div>`));
-      ($(".userDetail h1")).after($userTweet);
+      var $userTweet = $('<section class = "userTweet tweet"></section>');
+      $userTweet.append($(`<p class="tweetContent">${tweet.message}</p>`));
+      $userTweet.append($(`<p class="tweetTime"> ${time} </p>`));
+      ($(".userDetail h2")).after($userTweet);
     
     }
-    $tweet.append($(`<div class="tweetContent"> ${content} </div>`));
-    $tweet.append($(`<div class="tweetTime"> ${time} </div>`));
-    $tweet.prependTo($feed);
+    $tweet.append($(`<p class="tweetContent"> ${content} </p>`));
+    $tweet.append($(`<p class="tweetTime"> ${time} </p>`));
+    $tweet.prependTo($feedContent);
     index++;
   }
 }
 
-$('.tweet a').on('click',function(){
+$('.feedContent').on('click',$('.tweet a'), function(event){
   
   if ($('.userDetail').length !==0){
     $('.userDetail').remove();
   };
   $(".feed").addClass("withUser");
-  $(".feed").after($("<div class='fix'><\div>)"));
-  var userName=$(this).data('name');
+
+  var userName= $(event.target).data('name');
   $userSection = $(`<aside class="userDetail" data-name=${userName}></aside>`);
-  $userSection.prepend($(`<h2> ${userName} </h2>` ));
+  $userSection.prepend($(`<h2> @${userName} </h2>` ));
   $(".header").after($userSection);
   var userObj = {};
   userObj=streams.users[userName];
@@ -61,16 +66,12 @@ $('.tweet a').on('click',function(){
     var $tweet = $('<section class = "userTweet tweet"></section>');
     var content = tweet.message;
     var time = tweet.created_at;
-    $tweet.append($(`<div class="tweetContent"> ${content} </div>`));
-    $tweet.append($(`<div class="tweetTime"> ${time} </div>`));
+    $tweet.append($(`<p class="tweetContent"> ${content} </p>`));
+    $tweet.append($(`<p class="tweetTime"> ${time} </p>`));
     ($(".userDetail h2")).after($tweet);
   }
   
 });
-  
-function addTweets(){
-  
-}  
   
 });
 
